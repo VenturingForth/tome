a43ce366656837f46a8213601fc50193f5822b7a
VenturingForth <joshua@codeadventure.uk>
2025-03-18 21:07:56 +0000

    issue-001: scaffold base command interfaces

---
+++ Application/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.Abstractions.Messaging;
+
+// Interface representing a command that does not have a return value
+public interface ICommand : IBaseCommand
+{
+    
+}
+
+// Interface representing a command that has a return value
+public interface ICommand<TResponse> : IBaseCommand
+{
+    
+}
+
+// An interface that can give a generic way of implementing the above two interfaces
+public interface IBaseCommand
+{
+    
+}
