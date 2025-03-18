87b7d659db45da5df9a1cc318e50654b196de630
VenturingForth <joshua@codeadventure.uk>
2025-03-18 22:10:49 +0000

    issue-001: implements command handler interface

--- Application/Abstractions/Messaging/ICommandHandler.cs
+++ Application/Abstractions/Messaging/ICommandHandler.cs
@@ -1,6 +1,15 @@
-﻿namespace Application.Abstractions.Messaging;
+﻿using SharedKernel;
 
-public class ICommandHandler
+namespace Application.Abstractions.Messaging;
+
+public interface ICommandHandler<in TCommand>
+    where TCommand : ICommand
 {
-    
+    Task<Result> Handle(TCommand command, CancellationToken cancellationToken);
+}
+
+public interface ICommandHandler<in TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+{
+    Task<Result<TResponse>> Handle(TCommand command, CancellationToken cancellationToken);
 }
