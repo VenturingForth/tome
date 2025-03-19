ffc5848873aa49ae8e01d5b5fbfad5f7664da608
VenturingForth <joshua@codeadventure.uk>
2025-03-19 16:10:21 +0000

    issue-001: add MediatR to command interfaces

--- Application/Abstractions/Messaging/ICommand.cs
+++ Application/Abstractions/Messaging/ICommand.cs
@@ -1,19 +1,16 @@
-﻿namespace Application.Abstractions.Messaging;
+﻿using MediatR;
+using SharedKernel;
+
+namespace Application.Abstractions.Messaging;
 
 // Interface representing a command that does not have a return value
-public interface ICommand : IBaseCommand
+public interface ICommand : IRequest<Result>
 {
-    
+     
 }
 
 // Interface representing a command that has a return value
-public interface ICommand<TResponse> : IBaseCommand
-{
-    
-}
-
-// An interface that can give a generic way of implementing the above two interfaces
-public interface IBaseCommand
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>
 {
     
 }
--- Application/Abstractions/Messaging/ICommandHandler.cs
+++ Application/Abstractions/Messaging/ICommandHandler.cs
@@ -1,14 +1,16 @@
-﻿using SharedKernel;
+﻿using MediatR;
+using SharedKernel;
 
 namespace Application.Abstractions.Messaging;
 
-public interface ICommandHandler<in TCommand>
+public interface ICommandHandler<in TCommand>: IRequestHandler<TCommand, Result>
     where TCommand : ICommand
 {
     Task<Result> Handle(TCommand command, CancellationToken cancellationToken);
 }
 
-public interface ICommandHandler<in TCommand, TResponse>
+public interface ICommandHandler<in TCommand, TResponse> 
+    : IRequestHandler<TCommand, Result<TResponse>>
     where TCommand : ICommand<TResponse>
 {
     Task<Result<TResponse>> Handle(TCommand command, CancellationToken cancellationToken);
